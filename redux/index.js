986940ccce3f6eb66b8758cb08fb534179c9231e
leo-mutuku <le.mutuku@gmail.com>
2023-09-20 05:54:00 +0300

    reduce working perfect

--- redux/index.js
+++ redux/index.js
@@ -1,4 +1,8 @@
-const BUY_CAKE = 'BUY_CAKE'
+const redux = require('redux')
+const createStore = redux.createStore
+
+const BUY_CAKE = "BUY_CAKE"
+const BUY_ICECREAM = "BUY_ICECREAM"
 
 function buyCake(){
     return{
@@ -8,20 +12,37 @@ function buyCake(){
         
     }
 }
-
-const initialState = {
-   numOfcakes:10
+function buyIcecream(){
+    return{
+        type:BUY_ICECREAM
+    }
 }
 
-const reducer = (state, action) =>{
+const initialState = {
+   numOfcakes:10,
+   numofIceCreams:20
+}
+
+
+const reducer = (state = initialState, action) =>{
     switch(action.type){
         case BUY_CAKE:return{
             ...state,
-           numberOfCakes: state.numberOfCakes - 1
+            numOfcakes: state.numOfcakes - 1
+        }
+        case BUY_ICECREAM:return{
+            ...state,
+           numofIceCreams: state.numofIceCreams - 1
         }
         default:return state
         }
-   
-    
-
 }
+
+const store = createStore(reducer)
+console.log(`intial state`, store.getState())
+const unsubscribe = store.subscribe(() => console.log("updated state", store.getState()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyIcecream())
+unsubscribe()
